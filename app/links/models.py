5dc6936c949c23309739f13da64758a60ba548fd
Max <ibroden@yandex.ru>
2024-06-30 13:00:33 +0300

    feat(4): (create, update, list) link endpoints

--- app/links/models.py
+++ app/links/models.py
@@ -1,5 +1,6 @@
 from sqlalchemy import Column, ForeignKey, Integer, String, DateTime, Uuid
 from sqlalchemy.orm import relationship
+from sqlalchemy.types import TIMESTAMP
 
 from core.database import Base
 
@@ -11,7 +12,7 @@ class Link(Base):
 
     short_id = Column(String, primary_key=True)
     redirect_url = Column(String)
-    expiration_date = Column(DateTime, nullable=True)
+    expiration_date = Column(type_=TIMESTAMP(timezone=True))
     redirects_limit = Column(Integer, nullable=True)
     redirects_left = Column(Integer, nullable=True)
     passphrase_hash = Column(String, nullable=True)
--- app/links/routes.py
+++ app/links/routes.py
@@ -8,6 +8,7 @@ from core.database import get_async_session
 from .schemas import LinkRead, LinkCreate, LinkUpdate
 
 from links.models import *
+from auth.database import User
 
 router = APIRouter(
     prefix='/links',
@@ -23,16 +24,36 @@ async def get_link(short_id: str,
 
 
 @router.post('/create', response_model=LinkRead)
-async def create_link(link: LinkCreate, user=Depends(current_user)):
-
-    pass
+async def create_link(link: LinkCreate, user=Depends(current_user),
+                      db: AsyncSession = Depends(get_async_session)):
+    newLink = Link(short_id=link.short_id,
+                   redirect_url=link.redirect_url,
+                   expiration_date=link.expiration_date,
+                   redirects_limit=link.redirects_limit,
+                   redirects_left=link.redirect_left,
+                   passphrase_hash=link.passphrase_hash,
+                   banner_id=link.banner_id,
+                   owner_id=link.owner_id)
+    db.add(newLink)
+    await db.commit()
+    await db.refresh(newLink)
+    return newLink
 
 
 @router.post('/{banner_id}', response_model=LinkRead)
-async def update_link(short_id: str, link: LinkUpdate, user=Depends(current_user)):
-    pass
+async def update_link(short_id: str, link: LinkUpdate,
+                      user=Depends(current_user),
+                      db: AsyncSession = Depends(get_async_session)):
+    db.query(Link).filter(Link.short_id == short_id).update(**LinkUpdate.dict())
+    res = db.query(Link).filter(Link.short_id == short_id).first()
+    await db.commit()
+    await db.refresh(res)
+    return res
 
 
-@router.get('/list', response_model=list[LinkRead])
-async def list_links(user_id: str, user=Depends(current_user)):
-    pass
+
+@router.get('/list', response_model=list[LinkRead], description="Lists links for current authorized user")
+async def list_links(user=Depends(current_user),
+                     db: AsyncSession = Depends(get_async_session)):
+    links = await db.query(User).filter(User.id == current_user.id).all()
+    return links
