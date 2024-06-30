7b555d3d50b4b3debf76dcfc706ec28195b0e515
Max <ibroden@yandex.ru>
2024-06-30 11:32:51 +0300

    feat(4): get link endpoint

--- app/links/routes.py
+++ app/links/routes.py
@@ -1,6 +1,13 @@
-from fastapi import APIRouter
+from typing import Annotated, AsyncGenerator
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from fastapi import APIRouter, Depends
+from auth import current_user
+from core.database import get_async_session
 
 from .schemas import LinkRead, LinkCreate, LinkUpdate
+
+from links.models import *
 
 router = APIRouter(
     prefix='/links',
@@ -9,20 +16,23 @@ router = APIRouter(
 
 
 @router.get('/{short_id}', response_model=LinkRead)
-async def get_link(short_id: str):
-    pass
+async def get_link(short_id: str,
+                   db: AsyncSession = Depends(get_async_session)):
+    link = await db.query(Link).filter(Link.short_id == short_id).first()
+    return link
 
 
 @router.post('/create', response_model=LinkRead)
-async def create_link(link: LinkCreate):
+async def create_link(link: LinkCreate, user=Depends(current_user)):
+
     pass
 
 
 @router.post('/{banner_id}', response_model=LinkRead)
-async def update_link(short_id: str, link: LinkUpdate):
+async def update_link(short_id: str, link: LinkUpdate, user=Depends(current_user)):
     pass
 
 
 @router.get('/list', response_model=list[LinkRead])
-async def list_links():
+async def list_links(user_id: str, user=Depends(current_user)):
     pass
