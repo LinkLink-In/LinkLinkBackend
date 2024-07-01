54520434a34feb630e90cae079eedae5c20e7a9e
Max <ibroden@yandex.ru>
2024-07-02 00:17:47 +0300

    feat(8): merge

--- app/main.py
+++ app/main.py
@@ -1,5 +1,4 @@
 from fastapi import FastAPI
-from starlette.middleware.cors import CORSMiddleware
 
 from auth.routes import *
 from auth.schemas import UserRead, UserCreate, UserUpdate
@@ -11,19 +10,6 @@ from banners.routes import router as banner_router
 from redirects.routes import router as redirect_router
 
 app = FastAPI(**APP_META)
-
-origins = [
-    "*"
-]
-
-
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=origins,
-    allow_credentials=False,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
 
 app.include_router(
     auth_router,
