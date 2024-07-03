aa17ecbc8245b29fe26d74636103b908b64726a8
anijackich <anijackich@gmail.com>
2024-07-03 13:07:40 +0300

    Code refactoring

--- app/links/routes.py
+++ app/links/routes.py
@@ -7,7 +7,7 @@ from core.database import get_async_session
 
 from .schemas import LinkRead, LinkCreate, LinkUpdate
 
-from links.models import *
+from . import models
 
 router = APIRouter(
     prefix='/links',
@@ -18,49 +18,47 @@ router = APIRouter(
 @router.get('/{short_id}', response_model=LinkRead)
 async def get_link(short_id: str,
                    db: AsyncSession = Depends(get_async_session)):
-    link = await db.get(Link, short_id)
-    return link
+    return await db.get(models.Link, short_id)
 
 
 @router.post('/create', response_model=LinkRead)
-async def create_link(link: LinkCreate, user=Depends(current_user),
+async def create_link(link: LinkCreate,
+                      user=Depends(current_user),
                       db: AsyncSession = Depends(get_async_session)):
-    newLink = Link(short_id=link.short_id,
-                   redirect_url=link.redirect_url,
-                   expiration_date=link.expiration_date,
-                   redirects_limit=link.redirects_limit,
-                   redirects_left=link.redirects_left,
-                   passphrase_hash=link.passphrase_hash,
-                   banner_id=link.banner_id,
-                   owner_id=user.id)
-    db.add(newLink)
+    db_link = models.Link(short_id=link.short_id,
+                          redirect_url=link.redirect_url,
+                          expiration_date=link.expiration_date,
+                          redirects_limit=link.redirects_limit,
+                          redirects_left=link.redirects_left,
+                          passphrase_hash=link.passphrase_hash,
+                          banner_id=link.banner_id,
+                          owner_id=user.id)
+    db.add(db_link)
+
     await db.commit()
-    await db.refresh(newLink)
-    return newLink
+    await db.refresh(db_link)
+
+    return db_link
 
 
 @router.put('/{short_id}', response_model=LinkRead)
 async def update_link(short_id: str, link: LinkUpdate,
                       user=Depends(current_user),
                       db: AsyncSession = Depends(get_async_session)):
-    res = await db.execute(update(Link).where(Link.short_id == short_id).values(**link.dict()))
+    await db.execute(
+        update(models.Link)
+        .where(models.Link.short_id == short_id)
+        .values(**link.dict())
+    )
+
     await db.commit()
-    return await db.get(Link, link.short_id)
+
+    return await db.get(models.Link, link.short_id)
 
 
 @router.get('/list/all', response_model=list[LinkRead])
 async def list_links(user=Depends(current_user),
                      db: AsyncSession = Depends(get_async_session)):
-    links = await db.execute(
-        select(Link).where(Link.owner_id == user.id)
-    )
-
-    links_result = links.scalars().all()
-    if not links_result:
-        raise HTTPException(status_code=404, detail="No links found")
-
-    # Map the result to your Pydantic model
-    links_read = [LinkRead.from_orm(link) for link in links_result]
-    return links_read
-
-
+    return [LinkRead.from_orm(link) for link in (await db.execute(
+        select(models.Link).where(models.Link.owner_id == user.id)
+    )).scalars().all()]
--- app/redirects/routes.py
+++ app/redirects/routes.py
@@ -20,42 +20,33 @@ router = APIRouter(
 @router.get('/{redirect_id}', response_model=RedirectRead)
 async def get_redirect(redirect_id: UUID,
                        db: AsyncSession = Depends(get_async_session)):
-    link = await db.get(models.Redirect, redirect_id)
-    return link
+    return await db.get(models.Redirect, redirect_id)
 
 
 @router.post('/create', response_model=RedirectRead)
 async def create_redirect(redirect: RedirectCreate,
                           db: AsyncSession = Depends(get_async_session)):
-    newRedirect = models.Redirect(link_id=redirect.link_id,
+    db_redirect = models.Redirect(id=uuid.uuid4(),
+                                  link_id=redirect.link_id,
                                   redirected_at=datetime.now(),
                                   ip=redirect.ip,
                                   user_agent=redirect.user_agent,
                                   referrer=redirect.referrer,
                                   browser=redirect.browser,
                                   platform=redirect.platform,
-                                  language=redirect.language,
-                                  id=uuid.uuid4(),
-                                  )
+                                  language=redirect.language)
 
-    db.add(newRedirect)
+    db.add(db_redirect)
+
     await db.commit()
-    await db.refresh(newRedirect)
+    await db.refresh(db_redirect)
 
-    return newRedirect
+    return db_redirect
 
 
 @router.get('/list/{short_id}', response_model=list[RedirectRead])
 async def list_redirects(short_id: str,
                          db: AsyncSession = Depends(get_async_session)):
-    redirects = await db.execute(
+    return [RedirectRead.from_orm(redirect) for redirect in (await db.execute(
         select(models.Redirect).where(models.Redirect.link_id == short_id)
-    )
-
-    redirects_result = redirects.scalars().all()
-    if not redirects_result:
-        raise HTTPException(status_code=404, detail="No links found")
-
-    # Map the result to your Pydantic model
-    redirects_read = [RedirectRead.from_orm(redirect) for redirect in redirects_result]
-    return redirects_read
+    )).scalars().all()]
